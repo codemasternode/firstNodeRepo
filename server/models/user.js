fcd9bc93d0c666df2deca74b86974c6f786802f1
javaxd123 <marcinwarzybok@outlook.com>
2018-04-29 12:45:31 +0200

    first contact with jwt

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,57 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
 
-var Schema = mongoose.Schema;
+var Schema = mongoose.Schema
 var userSchema = new Schema({
     email: {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'It is not valid email'
+        }
+    },
+    password: {
+        required: true,
+        type: String,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        }, token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
+userSchema.methods.toJSON = function () {
+    var user = this
+    var userObject = user.toObject()
+
+    return _.pick(userObject, ['_id', 'email'])
+}
+
+userSchema.methods.generateAuthToken = function () {
+    var user = this
+    var access = 'auth'
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc').toString()
+
+    user.tokens = user.tokens.concat([{
+        access, token
+    }])
+
+    return user.save().then(() => {
+        return token
+    })
+}
 
 var User = mongoose.model('User', userSchema);
 
