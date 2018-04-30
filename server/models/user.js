410ce392b8b396ce3b66e5a16eee4bc6c735e9c0
javaxd123 <marcinwarzybok@outlook.com>
2018-04-30 12:56:56 +0200

    login route created

--- server/models/user.js
+++ server/models/user.js
@@ -72,6 +72,28 @@ userSchema.statics.findByToken = function (token) {
 
 }
 
+
+userSchema.statics.findByCredentials = function (email, password) {
+    var user = this
+
+    return user.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject()
+        }
+
+        return new Promise((resolve, reject) => {
+            bcypt.compare(password,user.password, (err,res) => {
+                if(res) {
+                    resolve(user)
+                }else {
+                    reject()
+                }
+            })
+        })
+
+    })
+}
+
 userSchema.pre('save', function (next) {
     var user = this
 
@@ -88,6 +110,7 @@ userSchema.pre('save', function (next) {
 
 
 })
+
 
 
 var User = mongoose.model('User', userSchema);
